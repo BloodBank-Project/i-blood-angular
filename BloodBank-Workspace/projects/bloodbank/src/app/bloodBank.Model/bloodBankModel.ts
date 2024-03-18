ade4e3b1753eeef28f053a37f03b3114e88ae7b2
EdigaJyoti <152795576+EdigaJyoti@users.noreply.github.com>
2024-03-18 22:11:26 +0530

    angular commit

---
+++ BloodBank-Workspace/projects/bloodbank/src/app/bloodBank.Model/bloodBankModel.ts
@@ -0,0 +1,11 @@
+export class BloodBank {
+    constructor(
+        public bloodBankId: number,
+        public bloodBankName: string,
+        public bloodBankLocation: string,
+        public availableBloodGroup: string,
+        public bloodQuantity: string,
+        public bloodGroup: string = ""
+
+    ) { }
+}
